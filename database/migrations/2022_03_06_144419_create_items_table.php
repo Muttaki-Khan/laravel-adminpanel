7c9ad2397cf6cb9aa87c87e9c51af7ea6916516f
Md Habib Al Muttakii Khan <m04khan1995@gmail.com>
2022-03-06 22:24:12 +0600

    first commit
    this is the first commit

---
+++ database/migrations/2022_03_06_144419_create_items_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 50);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
